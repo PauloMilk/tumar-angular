800cd5b582c0972fe4c3bdafd7144e5b9af06991
Paulo Costa <pauloleitecosta14@gmail.com>
2023-04-11 22:03:34 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Item} from "./item";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+
+  filter: 'all' | 'active' | 'done' = 'all';
+   allItems: Item[] = [
+    { description: 'eat', done: true },
+    { description: 'sleep', done: true },
+    { description: 'play', done: false },
+    { description: 'laugh', done: false },
+  ];
+
+  get items() {
+    if(this.filter == 'all') {
+      return this.allItems;
+    }
+    return this.allItems.filter((item) => this.filter == 'done' ? item.done : !item.done);
+  }
+
+  addItem(description: string) {
+    this.allItems.unshift({
+      description,
+      done: false
+    })
+  }
+
+  delete(item: Item) {
+    this.allItems.splice(this.allItems.indexOf(item), 1)
+  }
 }
